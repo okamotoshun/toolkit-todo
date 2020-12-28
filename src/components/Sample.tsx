4e03a0cf4bc451723a8d206567b141c3decb56a0
okamotoshun <okamons8811@gmail.com>
2020-12-28 21:56:36 +0900

    toolkit start

---
+++ src/components/Sample.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sample: React.FC = () => {
+  return (
+    <div>
+      <button className='button' onClick={() => console.log('button')}>
+        button
+      </button>
+    </div>
+  );
+};
+
+export default Sample;
